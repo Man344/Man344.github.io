da77b2436167e41abf7bb50cb15817c22b001b98
Man344 <mathis.allen@epsi.fr>
2021-02-21 18:25:49 +0100

    ChartDEV

--- js/competences.js
+++ js/competences.js
@@ -3,17 +3,18 @@ let myFirstChart = $('#myChart');
 let myChart = new Chart(myFirstChart, {
     type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [8, 6, 7, 5, 6, 3, 1],
+            label : "COUCU",
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(0, 255, 0,0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -21,18 +22,12 @@ let myChart = new Chart(myFirstChart, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 159, 64, 1)',
+                'rgba(0, 255, 0, 1)'
             ],
             borderWidth: 1
-        }]
+        }],
+        labels: ['PHP (Symfony)', 'Javascript', 'C# (Entity Framework)'
+        , 'Dart (Flutter)', 'Scala (Play)', 'C', 'JAVA'],
     },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
 });
