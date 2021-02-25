b1f0898faaaa812b9df949922afbc75a1b3cae7c
Man344 <mathis.allen@epsi.fr>
2021-02-25 17:56:22 +0100

    CONTENT END

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,15 @@
-let textToOutput = "Hello World";
-
-let shelljs = new Shell('#typed',{
-  typed:Typed,
-  loop:true,
-  responsive:true,
-  typeSpeed: 10,
-  style: 'ubuntu',
-  theme: 'dark',
-  user: 'root',
-  host: 'man344',
-  commands: [{"input":"cat hello.js", "output": "hello"},]
+document.addEventListener('DOMContentLoaded', function() {
+  let shelljs = new Shell('#typed',{
+    typed:Typed,
+    responsive:true,
+    typeSpeed: 10,
+    style: 'ubuntu',
+    theme: 'dark',
+    user: 'root',
+    host: 'man344',
+    commands:[
+    "Hi recruiters !",
+    {"input": "cat helloRectruiter.js", "output":[ "I will be happy to join your company"]},
+  ]
+  });
 });
