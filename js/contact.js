edd71d477feb0dc9f311e094663156bb39fbe150
Man344 <76881185+Man344@users.noreply.github.com>
2024-10-17 15:30:11 +0200

    update

--- js/contact.js
+++ js/contact.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
     host: 'kali',
     commands:[
       {"input": "cat mathis.txt", "output":["A l'écoute d'opportunités pouvant satisfaire ma soif d'apprendre :)"]},
-      {"input": "nmap dans-quel-domaine-je-veux-travailler.com", "output": [["STATE   SERVICE"],["open   cyber"],["open   dev backend (avec lien cyber)"]]}
+      {"input": "nmap -sV -A dans-quel-domaine-je-veux-travailler.com", "output": [["STATE   SERVICE"],["open   IAM"],["open   pentest"]]}
   ]
   });
 });
